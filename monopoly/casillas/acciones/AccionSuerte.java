a65196cdf2867b14f62266e1549a50cde0923ab0
albertortm <rodrigoalbertotorresmontes@gmail.com>
2024-12-07 18:55:17 +0100

    Push manual de AccionSuerte.java

--- monopoly/casillas/acciones/AccionSuerte.java
+++ monopoly/casillas/acciones/AccionSuerte.java
@@ -1,22 +1,33 @@
 package monopoly.casillas.acciones;
 
+import java.util.ArrayList;
+
+import monopoly.casillas.*;
+import monopoly.cartas.*;
 import partida.*;
+import monopoly.Texto;
 
 public class AccionSuerte extends Accion {
+    // ATRIBUTO EXTRA: BARAJA CORRESPONDIENTE
+    private ArrayList<CartaSuerte> baraja_suerte;
+
+    // CONSTRUCTOR
     public AccionSuerte(String nombre, int posicion) {
-        super(nombre,posicion);
-    }
-    
-    public boolean evaluarCasilla(Jugador jugadorActual, int tirada){
-        return true;
+        super(nombre, posicion);
+
+        // Añadimos las cartas de la baraja suerte
+        this.baraja_suerte = new ArrayList<CartaSuerte>();
+        this.baraja_suerte.add(new CartaSuerte(Texto.CARTA_SUERTE_1, 1));
+        this.baraja_suerte.add(new CartaSuerte(Texto.CARTA_SUERTE_2, 2));
+        this.baraja_suerte.add(new CartaSuerte(Texto.CARTA_SUERTE_3, 3));
+        this.baraja_suerte.add(new CartaSuerte(Texto.CARTA_SUERTE_4, 4));
+        this.baraja_suerte.add(new CartaSuerte(Texto.CARTA_SUERTE_5, 5));
+        this.baraja_suerte.add(new CartaSuerte(Texto.CARTA_SUERTE_6, 6));
     }
 
-    public String infoCasilla(){
-        String info = "{\n";
-        info += "\tTipo: Accion\n";
-        info += String.format("\tDeberás escoger una entre 6 cartas\n");
-        info += jugadoresEnCasilla();
-        info += "}\n";
-        return info;
+    // GETTER DEL TIPO DE BARAJA PARA cogerCarta() en Accion.java
+    @Override
+    protected ArrayList<CartaSuerte> getBaraja() {
+        return this.baraja_suerte; // Return the specific deck for AccionSuerte
     }
 }
